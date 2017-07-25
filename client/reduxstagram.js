08d5f287584e6bf3d97474ee27b1cf961501f41c
kerembakir <kerembakir@gmail.com>
2017-07-25 14:19:00 +0200

    Create App.js, accessing Dispatch and State with Redux

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -4,7 +4,7 @@ import { render } from 'react-dom'
 import css from './styles/style.styl'
 
 //import Components
-import Main from './components/Main'
+import App from './components/App'
 import Single from './components/Single'
 import PhotoGrid from './components/PhotoGrid'
 
@@ -16,14 +16,14 @@ import store, { history } from './store'
 
 const router = (
   <Provider store={store}>
-  <Router history={history}>
-    <Route path="/" component={Main}>
-      <IndexRoute component={PhotoGrid}></IndexRoute>
-      <Route path="/view/:postId" component={Single}></Route>
-    </Route>
-  </Router>
+    <Router history={history}>
+      <Route path="/" component={App}>
+        <IndexRoute component={PhotoGrid}> </IndexRoute>
+        <Route path="/view/:postId" component={Single}></Route>
+      </Route>
+    </Router>
   </Provider>
-  )
+)
 
 
 render(router, document.getElementById('root'))
